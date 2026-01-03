4fa5ae8f683a94263c5ef9c587198379cc8712e6
robertoferreira2804 <roberto.ferreira14@gmail.com>
2026-01-03 17:18:58 -0300

    Add files via upload

--- R/Program_C_MCS.R
+++ R/Program_C_MCS.R
@@ -1,34 +1,40 @@
-library (pracma)
-clear ()
-tic()
-n=5 #Sample size
-target_alpha<-0.002699796 #Desired significance level
-lower_target_alpha<-target_alpha/2
-upper_target_alpha<-1-target_alpha/2
-corridas=10000000
+rm(list = ls())
+t0 <- Sys.time()
+
+n <- 5 #Sample size
+alpha <- 2 * (1 - pnorm(3)) # Desired significance level = 0.002699796
+lower_alpha <- alpha / 2
+upper_alpha <- 1 - alpha / 2
+runs <- 10000000
+
 #In-Control
-k<-5.4 #shape
-lambda=2.2 #scale
+lambda_0  <- 2.2 #scale
+k_0 <- 5.4 #shape
+
 #out-of-control
-k1=5.4 #shape
-lambda1=2.2 #scale lambda1=1.9
+lambda_1 <- 1.9 #scale to calculate ARL_1
+k_1 <- 5.4 #shape
 set.seed(12345)
 
-R<-(matrix(rweibull(n*corridas,k,lambda),n,corridas)) 
-Rsum=colSums(R)
-LCL=quantile(Rsum,lower_target_alpha)
-UCL=quantile(Rsum,upper_target_alpha)
-ARL0=1/(mean(Rsum<LCL)+mean(Rsum>UCL))
+R <- (matrix(rweibull(n*runs,k_0,lambda_0),n,runs)) #to calculate control limits
+Rsum  <- colSums(R)
+LCLy <- quantile(Rsum, lower_alpha)
+UCLy  <- quantile(Rsum, upper_alpha)
 
-R1<-(matrix(rweibull(n*corridas,k1,lambda1),n,corridas)) 
-Rsum1=colSums(R1)
-ARL1=1/(mean(Rsum1<LCL)+mean(Rsum1>UCL))
+R0 <- (matrix(rweibull(n*runs,k_0,lambda_0),n,runs)) #another database to calculate ARL_0
+Rsum0  <-  colSums(R0)
+ARL_0  <-  1 / (mean(Rsum0 < LCLy) + mean(Rsum0 > UCLy))
 
-cat('LCLy=', LCL, "\n")
-cat('UCLy=', UCL, "\n")
-cat('ARL0=', ARL0, "\n")
-cat('ARL1=', ARL1, "\n")
-toc()
+R1 <- (matrix(rweibull(n*runs,k_1,lambda_1),n,runs)) #to calculate ARL_1
+Rsum1  <-  colSums(R1)
+ARL_1  <-  1 / (mean(Rsum1 < LCLy) + mean(Rsum1 > UCLy))
+
+cat('LCLy = ', LCLy, "\n")
+cat('UCLy = ', UCLy, "\n")
+cat('ARL0 = ', ARL_0, "\n")
+cat('ARL1 = ', ARL_1, "\n")
+
+Sys.time() - t0
 
 
 
